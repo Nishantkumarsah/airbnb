36206e0e83346692b8dc1ed93705b2c6cbfc5ef2
Nishant kumar <146699018+Nishantkumarsah@users.noreply.github.com>
2024-12-04 15:17:39 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -4,13 +4,19 @@ const wrapAsync = require("../utils/wrapAsyc.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
-const listingController = require("../controllers/listings.js")
+const listingController = require("../controllers/listings.js");
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 
 router
 .route("/")
 .get(wrapAsync(listingController.index))
-.post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+// .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+.post( upload.single('listing[image]'), (req, res)=> {
+    res.send(req.file);
+});
 
 //New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
